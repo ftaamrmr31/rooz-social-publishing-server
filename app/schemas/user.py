9c726237b342558c6506ffe83eae50de04527821
ftaamrmr31 <ftaamrmr@gmail.com>
2026-03-09 04:48:59 +0300

    Refactor UserCreate model configuration

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel
 from datetime import datetime
+from pydantic import BaseModel, ConfigDict
 
 
 class UserCreate(BaseModel):
@@ -13,5 +13,4 @@ class UserRead(BaseModel):
     full_name: str
     created_at: datetime
 
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
